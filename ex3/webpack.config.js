ac1fdd25ef6a646118e25f43d337da9b51d99df1
junwoo45 <sinyoorang@gmail.com>
2019-07-15 15:31:26 +0900

    vendor와 optimization사용

---
+++ ex3/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './app/index.js',
+    vendor: [
+      'moment',
+      'lodash'
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true,
+        },
+      },
+    }
+  }
+}
