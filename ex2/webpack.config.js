3be61a541deea5d10f3c7214ea561d674086bcac
junwoo45 <sinyoorang@gmail.com>
2019-07-15 14:07:30 +0900

    extract-text-webpack-plugin 설치

---
+++ ex2/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    testName: './app/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader"
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
+}
