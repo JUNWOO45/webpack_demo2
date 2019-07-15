f6be8085faeb616c9afda9f4e2f884a1d6af7d7f
nunu <junwoo45@jaranda.kr>
2019-07-15 12:05:06 +0900

    CDN사용해서 lodash사용하던걸 npm으로 lodash설치 후, webpack으로 번들링 후 Import해서 사용 테스트

--- ex1/app/index.js
+++ ex1/app/index.js
@@ -1,1 +1,12 @@
-document.querySelector('#myTarget').innerText = "changed!!";
+import _ from 'lodash';
+
+function component () {
+  var element = document.createElement('div');
+
+  /* lodash is required for the next line to work */
+  element.innerHTML = _.join(['Hello','webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
